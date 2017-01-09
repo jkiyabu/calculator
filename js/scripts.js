af4ba1ff9aa0a8cfb1362edda1b4e5d8367e7fca
Jayeson Kiyabu and Justin Stofiel <justin@>
2017-01-09 11:28:01 -0800

    add temperature calculator

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,20 @@ var number1 = parseFloat(prompt("What is your weight in lbs?"));
 var number2 = parseFloat(prompt("What is your height in ft?"));
 
 alert(bmi(number1, number2));
+
+//temperature calculator
+var fahrenheit = function(number1) {
+	return ((number1 - 32) * 5) / 9;
+};
+
+var celsius = function(number2) {
+	return ((number2 * 9) / 5) + 32;
+};
+
+
+var number1 = parseFloat(prompt("Input a temperature in fahrenheit"));
+
+var number2 = parseFloat(prompt("Input a temperature in celsius"));
+
+alert(fahrenheit(number1));
+alert(celsius(number2));
